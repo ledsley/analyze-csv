24a3608d010d9dd784c7dccb03ed52e6033f9e75
ledsley <lesha.sidorinko@yandex.ru>
2026-09-09 01:57:28 +0300

    перед переделкой функций с числовыми аргументами в одну функцию

--- csv_read.py
+++ csv_read.py
@@ -1,44 +1,236 @@
 import csv
 from csv_data import columns_dict
 
-#with open('cars_dataset.csv', 'r', newline='') as csv_file:
-#    csv_reader = csv.reader(csv_file)
-    
 def show_column(i):
         print(columns_dict[i])
             
-def filter_def(value):
-    with open('cars_dataset.csv','r', newline='') as  file:    #переделать функцию чтобы убрать отркытие файла
-        reader = csv.reader(file)
-        k=0
-        for row in reader:
-           s_row = set(row)
-           if value in s_row:
-               k+=1
-               print(row)
-        if k ==0:
-            print('По заданному фильтру ничего не нашлось')
-            
 def filter_price(s):
-    
-    indexes = []
-    
+    i_price = []
     if s[0] == '<':
         if s[1] == '=':
             
             value = int(s[2:])
             
-            for r in columns_dict['price_rub']:
+            for i,r in enumerate(columns_dict['price_rub']):
                 if int(r) <= value:
-                    i = columns_dict['price_rub'].index(r)
-                    print([columns_dict[k][i] for k in columns_dict])
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_price.append(i)
             
         else:
             
             value = int(s[1:])
             
-            for r in columns_dict['price_rub']:
+            for i,r in enumerate(columns_dict['price_rub']):
                 if int(r) < value:
-                    i = columns_dict['price_rub'].index(r)
-                    print([columns_dict[k][i] for k in columns_dict])  # повторить для остальных операторов
-          
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_price.append(i)
+                    
+    if s[0] == '>':
+        if s[1] == '=':
+            value = int(s[2:])
+            
+            for i,r in enumerate(columns_dict['price_rub']):
+                if int(r) >= value:
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_price.append(i)
+            
+        else: 
+            value = int(s[1:])
+            
+            for i,r in enumerate(columns_dict['price_rub']):
+                if int(r) > value:
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_price.append(i)
+                    
+    if s[0] == '=':
+        
+        if s[1] == '=':value = int(s[2:])
+            
+        else:value = int(s[1:])
+
+        for i,r in enumerate(columns_dict['price_rub']):
+            if int(r) == value:
+                #print([columns_dict[k][i] for k in columns_dict])
+                i_price.append(i)
+                    
+                    
+    if s[0].isdigit():
+        value = int(s)
+        for i,r in enumerate(columns_dict['price_rub']):
+            if int(r) == value:
+                #print([columns_dict[k][i] for k in columns_dict])
+                i_price.append(i)
+                
+    return i_price
+                    
+def filter_year(s):
+    i_year = []
+    if s[0] == '>':
+        if s[1] == '=':
+            v = int(s[2:])
+            
+            for i,r in enumerate(columns_dict['year']):
+                        if int(r) >= v:
+                            #print([columns_dict[k][i] for k in columns_dict])
+                            i_year.append(i)
+                        
+        else: 
+            v = int(s[1:])
+            for i,r in enumerate(columns_dict['year']):
+                if int(r) > v:
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_year.append(i)
+                    
+    if s[0] == '<':
+        if s[1] == '=':
+            v = int(s[2:])
+            
+            for i,r in enumerate(columns_dict['year']):
+                if int(r) <= v:
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_year.append(i)
+            
+        else:
+            v = int(s[1:])
+            for i,r in enumerate(columns_dict['year']):
+                if int(r) < v:
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_year.append(i)
+                    
+    if s[0] == '=':
+        if s[1] == '=':v = int(s[2:])
+            
+        else: v = int(s[1:])
+            
+        for i,r in enumerate(columns_dict['year']):
+            if int(r) == v:
+                #print([columns_dict[k][i] for k in columns_dict])
+                i_year.append(i)
+                
+    if s[0].isdigit():
+        v = int(s)
+        
+        for i,r in enumerate(columns_dict['year']):
+            if int(r) == v:
+                #print([columns_dict[k][i] for k in columns_dict])
+                i_year.append(i)
+                
+    return i_year
+                
+def filter_mileage(s):
+    i_mileage = []
+    if s[0] == '>':
+        if s[1] == '=':
+            v = int(s[2:])
+            for i,r in enumerate(columns_dict['mileage_km']):
+                if int(r) >= v:
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_mileage.append(i)
+            
+        else:
+            v = int(s[1:])
+            
+            for i,r in enumerate(columns_dict['mileage_km']):
+                if int(r) > v:
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_mileage.append(i)
+                    
+    if s[0] == '<':
+        if s[1] == '=':
+            v = int(s[2:])
+            
+            for i,r in enumerate(columns_dict['mileage_km']):
+                if int(r) <= v:
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_mileage.append(i)
+            
+        else:
+            v = int(s[1:])
+            
+            for i,r in enumerate(columns_dict['mileage_km']):
+                if int(r) < v:
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_mileage.append(i)
+                    
+    if s[0] == '=':
+        if s[1] == '=': v = int(s[2:])
+            
+        else: v = int(s[1:])
+        
+        for i,r in enumerate(columns_dict['mileage_km']):
+            if int(r) == v:
+                #print([columns_dict[k][i] for k in columns_dict])
+                i_mileage.append(i)
+                
+    if s[0].isdigit():
+        v = int(s)
+        
+        for i,r in enumerate(columns_dict['mileage_km']):
+            if int(r) == v:
+                #print([columns_dict[k][i] for k in columns_dict])
+                i_mileage.append(i)
+    
+    return i_mileage
+                
+def filter_eng(s):
+    i_engine = []
+    if s[0] == '>':
+        if s[1] == '=':
+            v = float(s[2:])
+            
+            for i,r in enumerate(columns_dict['engine_l']):
+                if float(r) >= v:
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_engine.append(i)
+            
+        else:
+            v = float(s[1:])
+            
+            for i,r in enumerate(columns_dict['engine_l']):
+                if float(r) > v:
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_engine.append(i)
+    
+    if s[0] == '<':
+        if s[1] == '=':
+            v = float(s[2:])
+            for i,r in enumerate(columns_dict['engine_l']):
+                if float(r) <= v:
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_engine.append(i)
+            
+        else: 
+            v = float(s[1:])
+            for i,r in enumerate(columns_dict['engine_l']):
+                if float(r) < v:
+                    #print([columns_dict[k][i] for k in columns_dict])
+                    i_engine.append(i)
+                    
+    if s[0] == '=':
+        if s[1] == '=':v = float(s[2:])
+            
+        else: v =float(s[1:])
+        
+        for i,r in enumerate(columns_dict['engine_l']):
+            if float(r) == v:
+                #print([columns_dict[k][i] for k in columns_dict])
+                i_engine.append(i)
+                
+    if s[0].isdigit():
+        v = float(s)
+        for i,r in enumerate(columns_dict['engine_l']):
+            if float(r) == s:
+                #print([columns_dict[k][i] for k in columns_dict])
+                i_engine.append(i)
+                
+    return i_engine
+                      
+#переделать функции в одну общую подавая в нее 2 значения( строка, ключ) #filter_price(args.price, 'price_rub') мб так\
+    
+def filter_str_args(s, column_name):
+    indexes = []
+    for i,r in enumerate(columns_dict[column_name]):
+        if r == s:
+            indexes.append(i)
+    print(len(indexes)) 
+    return indexes
