5c0ef08cb2bfa67cd475d8d9aad0d453cd2c3e96
ledsley <lesha.sidorinko@yandex.ru>
2026-09-09 20:24:20 +0300

    закончен механизм фильтарции

--- csv_data.py
+++ csv_data.py
@@ -2,19 +2,7 @@ import csv
 
 from collections import defaultdict
 
-with open('cars_dataset.csv','r', encoding='utf-8-sig') as csv_file:
-    csv_reader = csv.reader(csv_file,delimiter=',')
-    
-    columns_list = next(csv_reader)
-    
-    columns_set = set(columns_list)
-    
-#print(columns_set)
-
-#print(columns_list)
-
 with open('cars_dataset.csv','r', encoding='utf-8-sig') as file:
-    #reader = csv.DictReader(file)
     
     columns_l = [*csv.DictReader(file)]
 
@@ -24,4 +12,3 @@ for d in columns_l:
         columns_dict[k].append(v)
 
 columns_dict = dict(columns_dict)    
-#print(columns_dict) # списки значений в choices
--- csv_read.py
+++ csv_read.py
@@ -4,7 +4,7 @@ import operator
 def show_column(i):
         print(columns_dict[i])
 
-def filter_num_args_new(s,column_name,value_type):
+def filter_num_args(s,column_name,value_type):
     indexes = []
     op = []
     value=[]
@@ -29,17 +29,13 @@ def filter_num_args_new(s,column_name,value_type):
     
     for i,r in enumerate(columns_dict[column_name]):
         if ops[op](value_type(r),value) == True:
-            print([columns_dict[k][i] for k in columns_dict])
             indexes.append(i)
     
     return indexes
-                      
-#переделать функции в одну общую подавая в нее 2 значения( строка, ключ) #filter_price(args.price, 'price_rub') мб так\
     
 def filter_str_args(s, column_name):
     indexes = []
     for i,r in enumerate(columns_dict[column_name]):
         if r == s:
-            indexes.append(i)
-    print(len(indexes)) 
+            indexes.append(i) 
     return indexes
