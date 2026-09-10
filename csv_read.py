d865585ee88379e01b532674b4c2eb973d4a5ef3
ledsley <lesha.sidorinko@yandex.ru>
2026-09-10 04:04:13 +0300

    переделать архитектуру группировки

--- csv_read.py
+++ csv_read.py
@@ -1,5 +1,6 @@
 from csv_data import columns_dict
 import operator
+from collections import Counter
 
 def show_column(i):
         print(columns_dict[i])
@@ -39,3 +40,20 @@ def filter_str_args(s, column_name):
         if r == s:
             indexes.append(i) 
     return indexes
+
+def stats_c(s):
+    if '.' in s[0]:
+        data_type = float
+    else: data_type = int
+    
+    s = [data_type(x) for x in s]
+    n = len(s)
+    print(f'количество:{n}')
+    print(f'максимум:{max(s)}')
+    print(f'минимум:{min(s)}')
+    print(f'срденее значение:{round(sum(s)/n,1)}')
+    
+def grouping(column_name):
+    s = dict(Counter(columns_dict[column_name]))
+    return s
+#группировка + статитсика ключ из словаря это значение из списка по которому мне нужно пройтись и взять индексы и посчитать по этому индексу
--- pars.py
+++ pars.py
@@ -1,11 +1,11 @@
 import argparse
 
-from csv_read import show_column, filter_num_args, filter_num_args, filter_str_args
+from csv_read import show_column, filter_num_args, filter_str_args, stats_c,grouping
 
 from csv_data import columns_dict
 
 parser = argparse.ArgumentParser(prog= 'CSV_Analyze')
-subparsers = parser.add_subparsers(help = 'sub')
+subparsers = parser.add_subparsers(dest ='command',help = 'sub')
 
 parser.add_argument("--column", choices= (columns_dict.keys()), help='Введите название колонки')
 
@@ -20,6 +20,13 @@ sub_filter.add_argument('--transmission', choices=(set(columns_dict.get('transmi
 sub_filter.add_argument('--city', choices=(set(columns_dict.get('city'))), help='Введите город поиска')
 sub_filter.add_argument('--price', type=str, help='Введите оператор и цену без пробела')
 
+sub_stats = subparsers.add_parser('stats', help= 'Введите название колонки, статистику которой хотите увидеть')
+sub_stats.add_argument('--column_n', choices=('year','mileage_km','engine_l','price_rub'), help='Введите название колонки, по которой вы хотите посмотреть статистику')
+
+sub_group = subparsers.add_parser('grouping', help= ' Введите название колонки, по которой хотите получить группировку')
+sub_group.add_argument('--simple_group', choices=('brand','model','fuel','transmission','city'))
+sub_group.add_argument('--combined_w_stat',choices=('year','mileage_km','engine_l','price_rub'), help='Введите название колонки, по которой вы хотите посмотреть статистику')
+
 parser.print_help()
 
 args = parser.parse_args()
@@ -30,43 +37,70 @@ if args.column:
         show_column(i)
     else : print('колонки не существует')
     
-final_indexes = []
- 
-if args.brand:
-    final_indexes.append(filter_str_args(args.brand, 'brand'))
+if args.command == 'f':
+     
+    final_indexes = []
     
-if args.model:
-    final_indexes.append(filter_str_args(args.model, 'model'))
-    
-if args.fuel:
-    final_indexes.append(filter_str_args(args.fuel,'fuel'))
-    
-if args.city:
-    final_indexes.append(filter_str_args(args.city, 'city'))
-    
-if args.price:
-    final_indexes.append(filter_num_args(args.price,'price_rub',int))
-    
-if args.year:
-    final_indexes.append(filter_num_args(args.year,'year',int))
-    
-if args.mileage:
-    final_indexes.append(filter_num_args(args.mileage,'mileage',int))
-    
-if args.engine_l:
-    final_indexes.append(filter_num_args(args.engine_l,'engine_l',float))
-    
-if args.transmission:
-    final_indexes.append(filter_str_args(args.transmission,'transmission'))
+    if args.brand:
+        final_indexes.append(filter_str_args(args.brand, 'brand'))
+        
+    if args.model:
+        final_indexes.append(filter_str_args(args.model, 'model'))
+        
+    if args.fuel:
+        final_indexes.append(filter_str_args(args.fuel,'fuel'))
+        
+    if args.city:
+        final_indexes.append(filter_str_args(args.city, 'city'))
+        
+    if args.price:
+        final_indexes.append(filter_num_args(args.price,'price_rub',int))
+        
+    if args.year:
+        final_indexes.append(filter_num_args(args.year,'year',int))
+        
+    if args.mileage:
+        final_indexes.append(filter_num_args(args.mileage,'mileage',int))
+        
+    if args.engine_l:
+        final_indexes.append(filter_num_args(args.engine_l,'engine_l',float))
+        
+    if args.transmission:
+        final_indexes.append(filter_str_args(args.transmission,'transmission'))
+        
+    intersection_indexes =final_indexes[0]
 
-intersection_indexes =final_indexes[0]
+    n = len(final_indexes)
 
-n = len(final_indexes)
+    if n>1:
+        for i in range(1,len(final_indexes)):
+            set_i = set(final_indexes[i])
+            intersection_indexes = [x for x in intersection_indexes if x in set_i]
 
-if n>1:
-    for i in range(1,len(final_indexes)):
-        set_i = set(final_indexes[i])
-        intersection_indexes = [x for x in intersection_indexes if x in set_i]
+    for i in intersection_indexes:
+        print([columns_dict[k][i] for k in columns_dict])
+        
+elif args.command == 'stats':
+        l = columns_dict.get(args.column_n)
+        stats_c(args.column_n)
+        
+elif args.command == 'grouping':
+    if args.simple_group:
+        s = grouping(args.simple_group)
+        if args.combined_w_stat:
+            s_keys = list(s.keys())
+            indexes = []
+            for j in range(len(s_keys)):
+                in_j = []
+                for i,r in enumerate(columns_dict[args.simple_group]):
 
-for i in intersection_indexes:
-    print([columns_dict[k][i] for k in columns_dict])
+                    if s_keys[j] == r:
+                        in_j.append(i)
+                indexes.append(in_j)
+                        
+            for i in indexes:
+                s_for_stat = [columns_dict[args.combined_w_stat][j] for j in i]
+                print(s_keys[indexes.index(i)])
+                stats_c(s_for_stat)
+        
+        else: print(s)
